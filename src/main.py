904704320e02184ff7bb90afef7608d80e1f8fb1
somanath <somanath039@gmail.com>
2018-03-08 02:14:40 -0800

    dynamic input from JSON
    JSON Input for host

--- src/main.py
+++ src/main.py
@@ -5,9 +5,10 @@ from __future__ import print_function
 
 import sys
 import argparse
+import json
 
-from src.util import HostSession, Logger
-from src.core import Host
+#from src.util import HostSession, Logger
+#from src.core import Host
 
 
 def parse_args():
@@ -20,8 +21,15 @@ def main():
     Main Script
     """
     args = parse_args()
+    """
+    file = open('env_conf\host.json','r')
+    jsonData = file.read()
+    data = json.loads(jsonData)
+    print('Host: {}\nUserName: {}\nPassword: {}'.format(data['host_credentials'][0]['host'],data['host_credentials'][0]['user'], data['host_credentials'][0]['password']))
+    """
 
     sess = HostSession.HostSession().connect('10.107.182.18', 'root', 'ca$hc0w', False)
+    #sess = HostSession.HostSession().connect(data['host_credentials'][0]['host'], data['host_credentials'][0]['user'], data['host_credentials'][0]['password'] , False)
     if sess:
         stdin, stdout, stderr = sess.exec_command('esxcli system version get')
         for line in stdout:
