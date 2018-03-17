071456a312c5ed54750d9b4cc2901feef6fdb332
somanathr <somanath039@gmail.com>
2018-03-17 14:09:08 +0530

    changes in the VMconfig

--- src/core/vnf/VmTuning.py
+++ src/core/vnf/VmTuning.py
@@ -1,22 +1,38 @@
 """
-    This module deals with VM configurations, verification and other controls
+    There are a few configurations on the VM also that impact the overall networking throughput delivered.
+    1.Verify that exclusive affinity of all vCPU are enable.
+    2.Verify CPU reservation and share are reserved and made available to VM.
+    3.Verify Memory(RAM) reservation and share are reserved and made available to VM.
+    4.Verify Latency sensitivity is taking effect.
+    5.Verify the vNIC adapter type.
+    6.Verify the vNIC TX thread allocation is set.
+    7.Verify the SysContext is set to the no. of thread to be pinned.
+    8.Verify the vm NUMA is alligned to the NUMA of the physical NIC.
+
+    The above verification are done to ensure that it will not impact the overall networking throughput delivered.
+    Configuration are done if the verification fails.
+
 """
+
 from __future__ import print_function
-import paramiko
+# import paramiko
 import re
+from src.util import VmUtil
 
 __author__ = 'Somanath'
-
+"""
 host='10.107.182.19'
 username='root'
 password='ca$hc0w'
 datastore ='Datastore1'
 vmName= 'TEST-VM'
-result={}
-mat_list=[]
+"""
 
 
+
+"""
 # Function to Read .vmx file
+
 def readVMX(client, datastore, vmName):
     print('\nReading {}.vmx file'.format(vmName))
     stdin, stdout, stderr = client.exec_command(f'cat vmfs/volumes/{datastore}/{vmName}/{vmName}.vmx')
@@ -31,17 +47,20 @@ def get_vm_id(client, vmName):
 
 # Power off VM's
 def power_off_vm(client, vmName):
-    print(f'PowerOff: {vmName}')
+    # print(f'PowerOff: {vmName}')
     vmid = get_vm_id(client, vmName)
-    stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/power.off {}'.format(int(vmid)))
+    print(f'Vmid :{vmid}')
+    if vmid != '' or vmid != ' ':
+        stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/power.off {}'.format(int(vmid)))
 
 
 # Power on VM's
 def power_on_vm(client, vmName):
-    print(f'PowerOn: {vmName}')
+    #print(f'PowerOn: {vmName}')
     vmid = get_vm_id(client, vmName)
     stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/power.on {}'.format(int(vmid)))
 
+"""
 
 # Verify the NUMA Affinity
 def NUMAaffinity(client, vmnic='vmnic0'):
@@ -65,16 +84,16 @@ def add_parameter_vmx(client, datastore, vmName, data):
 
 # SET numa.nodeAffinity value
 def set_numa_NodeAffinity(client, datastore, vmName, numanode):
-    data = readVMX(client, datastore, vmName)
+    data = VmUtil.VmUtility.readVMX(client, datastore, vmName)
     data1 = f'numa.nodeAffinity = "{numanode}"'
-    power_off_vm(client, vmName)
+    VmUtil.VmUtility.power_off_vm(client, vmName)
     add_parameter_vmx(client, datastore, vmName, data1)
     replace_parameter_vmx(client, datastore, vmName, data1.replace('"', ''), data1)
-    power_on_vm(client, vmName)
+    VmUtil.VmUtility.power_on_vm(client, vmName)
 
 # Verify NUMA Affinity in config proprity
 def verify_numa_NodeAffinity(client,datastore, vmName):
-    print('verify the numa nodeAffinity')
+    #print('verify the numa nodeAffinity')
     data1 =f'numa.nodeAffinity = "{NUMAaffinity(client)}"'
     stdin , stdout, stderr = client.exec_command(f'cat vmfs/volumes/{datastore}/{vmName}/{vmName}.vmx | grep numa.node')
     numaNode = stdout.read().decode()
@@ -89,38 +108,41 @@ def verify_numa_NodeAffinity(client,datastore, vmName):
 
 # Verify the Vnic TX thread Allocation
 def verify_vnic_tx_thread(client, datastore, vmName):
-    print('\nVnic TX thread Alllocation')
+    # print('\nVnic TX thread Alllocation')
     stdin , stdout, stderr = client.exec_command(f'cat vmfs/volumes/{datastore}/{vmName}/{vmName}.vmx | grep ctxPerDev')
     return True if len(stdout.read().decode()) else False
 
 # Verify the SysContext
 def verify_SysContext(client, datastore, vmName):
-    print ('\nSys Context')
+    # print ('\nSys Context')
     stdin , stdout, stderr = client.exec_command(f'cat vmfs/volumes/{datastore}/{vmName}/{vmName}.vmx | grep sched.cpu.latencySensitivity.sysContexts')
     return True if len(stdout.read().decode()) else False
 
 # Verify the Vnic Adapter
 def vnic_adapter_type(client,datastore, vmName):
-    print ('\nVnic Adapter type (vmxnet) :')
+    # print ('\nVnic Adapter type (vmxnet) :')
     stdin , stdout, stderr = client.exec_command(f'cat vmfs/volumes/{datastore}/{vmName}/{vmName}.vmx | grep vmxnet')
     # print(stdout.read())
     return True if len(stdout.read()) != 0 else False
 
 # Verify the CPU pinning is proper
-def verify_cpuPinning():
+def verify_cpuPinning(mat_list, result):
     for key, value in result.items():
+        count=0
         for i in range(1, len(mat_list) - 1):
+            count += 1
             if key == mat_list[i][23] and value == mat_list[i][1]:
-                print('valid')
+                # print('valid')
                 break
-        # if i != (len(mat_list) - 1):
-        #    print('cpuid not pinned :{}'.format(key))
+        # return False if i != (len(mat_list) - 1) else True
+        if  count != (len(mat_list) - 1):
+            print('cpuid not pinned :{}'.format(key))
 
 # check the values are reservation are enabled
 def latency_value_check(latencySensitivity, exclaff):
     return True if latencySensitivity == -1 and exclaff > 0 else False
 
-def verify_latencySensitivity(self, client):
+def verify_latencySensitivity(client):
     """
     Verify the Latency Sensitivity feature
 
@@ -131,6 +153,7 @@ def verify_latencySensitivity(self, client):
     stdin, stdout, stderr = client.exec_command('net-stats -i 3 -t WicQv -A')
     d = eval(stdout.read().decode())
     data = d['stats'][0]['vcpus']
+    result = {}
     for i in data:
         if data[i]['name'] != 'net-stats':
             result[i] = int(data[i]['exclaff'])
@@ -142,13 +165,14 @@ def verify_latencySensitivity(self, client):
     stdin, stdout, stderr = client.exec_command('sched-stats -t pcpu-stats')
     schedStats = stdout.read().decode()
     data = re.sub(' +', ' ', schedStats).split('\n')
+    mat_list = []
     for i in range(len(data)):
         mat_list.append(data[i].split(' '))
-
+    # print(re.sub(' +', ' ', mat_list))
     # cross Verifing the Latency Sensitivity
-    verify_cpuPinning()
+    verify_cpuPinning(mat_list,result)
 
-def getSession():
+"""def getSession():
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
@@ -158,7 +182,7 @@ def getSession():
     except paramiko.SSHException as e:
         print(e.args)
         client.close()
+"""
+# client = getSession()
 
-client = getSession()
-
-client.close()
+# client.close()
--- src/tests/vm_config.py
+++ src/tests/vm_config.py
@@ -1,7 +1,29 @@
 from src.core.vnf import VmTuning
 from src.util import HostSession
+import json
 
+datastore = 'Datastore1'
+vmName = 'TEST-VM'
 def vm_config():
-    hosts = json.load(open(r'env_conf\host.json'))
+    hosts = json.load(open(r'..\env_conf\host.json'))
     for host in hosts['HOST_DETAILS']:
         client = HostSession.HostSession().connect(host['HOST'], host['USER'], host['PASSWORD'], False)
+        print(host['HOST'])
+        print(f'Latency Sensitivity set to High and Reserved :{VmTuning.verify_latencySensitivity(client)}')
+        # if VmTuning.verify_latencySensitivity(client) == False:
+        """
+               Config need to be done.
+        """
+        print(f'Vmnic Adapter type (vmxnet):{VmTuning.vnic_adapter_type(client,datastore,vmName)}')
+        print(f'vNIC TX thread allocation :{VmTuning.verify_vnic_tx_thread(client,datastore,vmName)}')
+        if VmTuning.verify_vnic_tx_thread(client,datastore,vmName) == False:
+            """
+            config for TX thread Allocation
+            """
+        print(f'Numa Affinity :{VmTuning.verify_numa_NodeAffinity(client,datastore,vmName)}')
+        print(f'system context : {VmTuning.verify_SysContext(client,datastore,vmName)}')
+
+        print(f'Vm config is Completed')
+        client.close()
+
+vm_config()
